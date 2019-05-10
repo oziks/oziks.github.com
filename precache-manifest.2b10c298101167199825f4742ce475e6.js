28967deabbe6b27253e28535e9118cfb43f4d031
Morgan Brunot <brunot.morgan@gmail.com>
2019-05-10 12:41:07 +0200

    Updates

---
+++ precache-manifest.2b10c298101167199825f4742ce475e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb2b8c98a8fc70a6cf461d7ead627e9e",
+    "url": "/static/media/Lora-Bold.eb2b8c98.ttf"
+  },
+  {
+    "revision": "ea0a7dc68773fba49c94",
+    "url": "/static/js/main.8fa5b5c5.chunk.js"
+  },
+  {
+    "revision": "940891f404f84a7749448af7acfa1528",
+    "url": "/static/media/user.940891f4.jpg"
+  },
+  {
+    "revision": "2d3327c51d3f917c61b83fd04c232bd3",
+    "url": "/static/media/Geomanist-Bold.2d3327c5.ttf"
+  },
+  {
+    "revision": "77cf7b2b6207fc251aabe76416703b1d",
+    "url": "/static/media/Geomanist-Regular.77cf7b2b.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "04f84cd510525dede3ddfadff7efb3f4",
+    "url": "/static/media/Lora-BoldItalic.04f84cd5.ttf"
+  },
+  {
+    "revision": "d36745bb0ed7cc14897bf9b1bc04203b",
+    "url": "/static/media/Lora-Italic.d36745bb.ttf"
+  },
+  {
+    "revision": "c87345ceb65eb56514768c598074a102",
+    "url": "/static/media/Lora-Regular.c87345ce.ttf"
+  },
+  {
+    "revision": "6ab65f8f47d7c2bee977",
+    "url": "/static/js/2.d77100de.chunk.js"
+  },
+  {
+    "revision": "860c7d57773aa24f2f1e5c7508719d0b",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Resume from "./Resume";
-
-ReactDOM.render(<Resume />, document.getElementById("root"));
